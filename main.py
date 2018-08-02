ad085de5b7738f860ed19086e17b35201680312a
tiffany foo <tffl288@gmail.com>
2018-08-02 15:04:32 -0700

    made changes to about and home

--- main.py
+++ main.py
@@ -138,7 +138,6 @@ class Profile(webapp2.RequestHandler):
         current_user = users.get_current_user()
         if current_user:
             current_email = current_user.email() #?
-            template = env.get_template('templates/profile.html')
         else:
             current_person = None
         logout_url = users.create_logout_url('/')
@@ -146,6 +145,7 @@ class Profile(webapp2.RequestHandler):
             'current_user': current_user,
             'logout_url': logout_url,
         }
+        template = env.get_template('templates/profile.html')
         self.response.write(template.render(templateVars))
 
 class CreateAccount(webapp2.RequestHandler):
@@ -202,8 +202,18 @@ class ResultsPage(webapp2.RequestHandler):
 
 class About(webapp2.RequestHandler):
     def get(self):
+        current_user = users.get_current_user()
+        if current_user:
+            current_email = current_user.email() #?
+        else:
+            current_person = None
+        logout_url = users.create_logout_url('/')
+        templateVars = {
+            'current_user': current_user,
+            'logout_url': logout_url,
+        }
         template = env.get_template("templates/about.html")
-        self.response.write(template.render())
+        self.response.write(template.render(templateVars))
 
 
 app = webapp2.WSGIApplication([
